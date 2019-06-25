d9879ea89cfb1b361d19d24eb4d0db247a72ef07
EMacco <emma4real37@gmail.com>
2019-06-25 13:08:28 +0100

    chore-[166897567]: Create Models and Migrations
    - Create models and migrations for user
    - Creat models and mogrations for relocation
    - Create models and migrations for monthly expenses
    - Create a hook to hash password before storing in the database
    
    [Finishes #166897567]

---
+++ server/models/monthlyexpense.js
@@ -0,0 +1,24 @@
+
+
+module.exports = (sequelize, DataTypes) => {
+  const MonthlyExpense = sequelize.define('MonthlyExpense', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    item: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    cost: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {});
+  return MonthlyExpense;
+};
